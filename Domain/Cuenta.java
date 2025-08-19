d9d4bbccf668fa599421719a512598691949085d
Shannell Bennit <bennitshannell@gmail.com>
2025-08-19 12:42:11 -0600

    Mi primer commit en java

---
+++ Domain/Cuenta.java
@@ -0,0 +1,66 @@
+package Domain;
+
+    public abstract class Cuenta {
+    // Attributes
+    private String accountNumber;
+    private double balance;
+    private Persona client;
+
+    // Constructor
+    public Cuenta(String accountNumber, double balance, Persona client) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.client = client;
+    }
+
+    public Cuenta() {
+        this.accountNumber = "";
+        this.balance = 0.0;
+        this.client = new Persona();
+    }
+
+    // Getters and Setters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Persona getClient() {
+        return client;
+    }
+
+    public void setClient(Persona client) {
+        this.client = client;
+    }
+    //Methods for polymorphism
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+    public abstract double interestCalculation();
+
+
+    @Override
+    public String toString() {
+        String result = "/nBANK ACCOUNT INFORMATION/n" +
+                "Account Number: " + this.getAccountNumber() + "/n" +
+                "Balance: " + this.getBalance() + "/n" +
+                "Account Owner: " + this.getClient().toString() + "/n";
+        return result;
+    }
+}
+
---
+++ IF2000_IntroduccionProgra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,13 @@
+package IF2000_IntroduccionProgra;
+
+public class IF2000_IntroduccionProgra {
+    public static void main(String[] args) {
+        String name = "Lionel";
+        int age = 40;
+
+        System.out.println("Hola Mundo");
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
+    }
+}
+
