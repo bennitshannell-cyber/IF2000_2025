a9778dd45930fe53265f9be84f4494f167e134da
Shannell Bennit <bennitshannell@gmail.com>
2025-08-21 10:37:10 -0600

    Main_IF2000_2025

---
+++ Logic/Main.java
@@ -0,0 +1,9 @@
+package Logic;
+
+public class Main {
+    public static void main(String[] args) {
+        Ejercicios_Semana2_PracticaEnClase ejercicios = new Ejercicios_Semana2_PracticaEnClase();
+        ejercicios.quiz2_A_primos();
+        ejercicios.quiz2_B_piramide(5);
+    }
+}
