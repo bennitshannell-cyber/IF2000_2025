6cd38e0a03722c759e71960022282d77b4a2ea6a
Shannell Bennit <bennitshannell@gmail.com>
2025-08-21 10:07:17 -0600

    PracticaEnClase_Semana2

--- Logic/Ejercicios_Semana2_PracticaEnClase.java
+++ Logic/Ejercicios_Semana2_PracticaEnClase.java
@@ -1,4 +1,23 @@
 package Logic;
 
 public class Ejercicios_Semana2_PracticaEnClase {
+
+    public void quiz2_A_primos() {
+        // Código del ejercicio A
+    }
+
+    public void quiz2_B_piramide(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Controla los espacios vacios
+            for (int j = 1; j <= n - i; j++) {
+                //Controla los *s de la piramide
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
