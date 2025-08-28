a9aec293fa33414ef2f11c5f81ca4308f6e12dd3
Shannell Bennit <bennitshannell@gmail.com>
2025-08-27 23:16:26 -0600

    Practica

--- Logic/Ejercicios_Semana2_PracticaEnClase.java
+++ Logic/Ejercicios_Semana2_PracticaEnClase.java
@@ -1,5 +1,7 @@
 package Logic;
 
+import java.util.Scanner;
+
 public class Ejercicios_Semana2_PracticaEnClase {
 
     public void quiz2_A_primos() {
@@ -21,17 +23,93 @@ public class Ejercicios_Semana2_PracticaEnClase {
         }
     }
     public void ejercicio_A(int n) {
-       for (int i = 1; i <= n; i++) {          
-        for (int j = 1; j <= n; j++) {       
-            if (i == 1 || i == n || j == 1 || j == n) {
-                // Si estamos en los bordes del cuadrado
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public void ejercicio_B(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
-            } else {
-                // Si estamos dentro del cuadrado
-                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public void ejercicio_C(int n) {
+        for (int i = n; i >= 1; i--) {             
+            for (int e = 0; e < (n - i) * 2; e++) { 
+            System.out.print(" ");
+            }
+                for (int j = 1; j <= i; j++) {          
+            System.out.print("*");
+        }
+        System.out.println();                   
+     }
+  } 
+  public void ejercicio_D(Scanner sc) {
+    int[] numeros = new int[10];
+    System.out.println("Ingrese 10 números enteros:");
+    for (int i = 0; i < 10; i++) {
+        numeros[i] = sc.nextInt();
+    }
+    // Ordenar el arreglo en orden ascendente
+    for (int i = 0; i < numeros.length - 1; i++) {
+        for (int j = 0; j < numeros.length - 1 - i; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                // Intercambiar
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
             }
         }
-        System.out.println();                
     }
-}
+    System.out.println("Números ordenados en orden ascendente:");
+    for (int num : numeros) {
+        System.out.print(num + " ");
+    }
+    System.out.println();
+  }
+    public void ejercicioE() {
+        int[] numeros = new int[100];
+        // Generar 100 números aleatorios entre 1 y 1000
+        for (int i = 0; i < 100; i++) {
+            numeros[i] = (int) (Math.random() * 1000) + 1;
+        }
+        // Ordenar el arreglo en orden descendente
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                if (numeros[j] < numeros[j + 1]) {
+                    // Intercambiar
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("100 números aleatorios ordenados en orden descendente:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    public void ejercicioF() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("B "); 
+                } else {
+                    System.out.print("N "); 
+                    // fichas negras
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- Logic/Main.java
+++ Logic/Main.java
@@ -31,7 +31,27 @@ public class Main {
          //Dueño de la cuenta//
         Persona p = new Persona("Yudhansen", "Paniagua", "3-0808-5656", "12123434", 20);
         System.out.println(p.toString());
-     
+
+        //Ejercicio_B
+        System.out.println("B) Triángulo decreciente\n");
+        eje.ejercicio_B(5); 
+
+        //Ejercicio_C
+        System.out.println("C) Triángulo desplazado a la derecha\n");
+        eje.ejercicio_C(5);
+
+        //Ejercicio_D
+        System.out.println("D) Ordenar 10 enteros\n");
+        eje.ejercicio_D(sc);
+
+        // Ejercicio_E
+        System.out.println("E) 100 enteros aleatorios ordenados descendentemente\n");
+        eje.ejercicioE();
+
+        // Ejercicio_F
+        System.out.println("F) Tablero de Damas\n");
+        eje.ejercicioF();
+        sc.close();
 
     }
 }
